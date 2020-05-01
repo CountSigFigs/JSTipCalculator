24ad2bebe2456edfbcc3b6f8094fe59ef56f1d98
Brian Holt <bharrisonholt@gmail.com>
2020-05-01 14:31:34 -0700

    fixing logic for modal

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const billValue = document.getElementById("billAmount")
-  const serviceQuality = document.getElementById("tipAmount")
-  const numberOfGuests = document.getElementById("amountGuests")
-  const tipDisplay = document.getElementById("output")
-  let tip = undefined
-  const tipCalculate = document.getElementById("submit")
-  
-  function tipCalculation() {
-      if (!billValue.value){
-          alert('Enter Your Bill Amount')
-          return;
-      }
-      if (!numberOfGuests.value){
-          alert('Please enter your guest')
-          return;
-      }
-      
-      if (isNaN(billValue.value)) {
-          alert("Please enter a valid number")
-      }
-      if (isNaN(numberOfGuests.value)) {
-          alert("Enter a valid number of guests")
-      }
-      let num = (billValue.value * serviceQuality.value) / numberOfGuests.value
-      let tip = num.toFixed(2)
-      if (numberOfGuests.value >= 2) {
-          tipDisplay.innerHTML = `Your tip is ${tip} each`
-      }
-      else { tipDisplay.innerHTML = `Your tip is ${tip}` }
-  
-  }
-  tipCalculate.addEventListener("click", tipCalculation);
-  
+const serviceQuality = document.getElementById("tipAmount")
+const numberOfGuests = document.getElementById("amountGuests")
+const tipDisplay = document.getElementById("output")
+let tip = undefined
+const tipCalculate = document.getElementById("submit")
+
+
+function tipCalculation() {
+    if (!billValue.value || isNaN(billValue.value)) {
+        tipDisplay.innerHTML = `Oops! Go back and make sure you entered 
+        only numbers for the bill value`
+        return;
+    }
+    if (!numberOfGuests.value || isNaN(numberOfGuests.value)) {
+        tipDisplay.innerHTML = `Oops! Go back and make sure you entered 
+        a number for the # of guests`
+        return;
+    }
+
+    let num = (billValue.value * serviceQuality.value) / numberOfGuests.value
+    let tip = num.toFixed(2)
+
+    if (numberOfGuests.value >= 2) {
+        tipDisplay.innerHTML = `Great! Your tip is $${tip} each`
+    }
+    else { tipDisplay.innerHTML = `Great! Your tip is $${tip}` }
+
+}
+
+tipCalculate.addEventListener("click", tipCalculation);
+
+
