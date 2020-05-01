e114cf7a9e269be4119cb14aa8a2e0b7758ddcd2
Brian Holt <bharrisonholt@gmail.com>
2020-04-30 23:44:41 -0700

    particles js

---
+++ app.js
@@ -0,0 +1,24 @@
+const billValue = document.getElementById("billAmount")
+  const serviceQuality = document.getElementById("tipAmount")
+  const numberOfGuests = document.getElementById("amountGuests")
+  const tipDisplay = document.getElementById("output")
+  let tip = undefined
+  const tipCalculate = document.getElementById("submit")
+  
+  function tipCalculation() {
+      if (isNaN(billValue.value)) {
+          alert("Please enter a valid number")
+      }
+      if (isNaN(numberOfGuests.value)) {
+          alert("Enter a valid number of guests")
+      }
+      let num = (billValue.value * serviceQuality.value) / numberOfGuests.value
+      let tip = num.toFixed(2)
+      if (numberOfGuests.value >= 2) {
+          tipDisplay.innerHTML = `Your tip is ${tip} each`
+      }
+      else { tipDisplay.innerHTML = `Your tip is ${tip}` }
+  
+  }
+  tipCalculate.addEventListener("click", tipCalculation);
+  
